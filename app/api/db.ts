82558af344e609b72ce1860e7e6b5cdd1ac7bc63
Atharva <dummy@gmail.com>
2025-03-25 01:43:47 +0530

    build-issue-resolved

---
+++ app/api/db.ts
@@ -0,0 +1,26 @@
+import { MongoClient, Db, ServerApiVersion } from "mongodb";
+
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
+
+export async function connectToDb() {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  await client.connect();
+
+  cachedClient = client;
+  cachedDb = client.db("ecommerce-nextjs");
+
+  return { client, db: client.db("ecommerce-nextjs") };
+}
